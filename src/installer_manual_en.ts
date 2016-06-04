f01071429ca4540ba48d5d4679046997aed21654
Tannin <sherb@gmx.net>
2016-06-04 20:37:42 +0200

    translation

---
+++ src/installer_manual_en.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>InstallDialog</name>
+    <message>
+        <location filename="installdialog.ui" line="20"/>
+        <source>Install Mods</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="32"/>
+        <source>New Mod</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="46"/>
+        <location filename="installdialog.cpp" line="235"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="62"/>
+        <source>Content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="72"/>
+        <source>Content of the archive. You can change the directory structure by using drag&amp;drop. Hint: Also try right clicking...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="75"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;This displays the content of the archive. &amp;lt;data&amp;gt; represents the base directory which will map to the game&apos;s data directory. You can change the base directory via the right-click context menu and you can move around files via drag&amp;amp;drop&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="118"/>
+        <source>Placeholder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="138"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.ui" line="145"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="181"/>
+        <source>Looks good</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="182"/>
+        <source>No problem detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="185"/>
+        <source>No game data on top level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="186"/>
+        <source>There is no esp/esm file or asset directory (textures, meshes, interface, ...) on the top level.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="235"/>
+        <source>Enter a directory name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="240"/>
+        <source>A directory with that name exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="267"/>
+        <source>Set data directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="268"/>
+        <source>Unset data directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="270"/>
+        <source>Create directory...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="272"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="287"/>
+        <source>Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installdialog.cpp" line="288"/>
+        <source>This mod was probably NOT set up correctly, most likely it will NOT work. You should first correct the directory layout using the content-tree.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallerManual</name>
+    <message>
+        <location filename="installermanual.cpp" line="58"/>
+        <source>Fallback installer for mods that can be extracted but can&apos;t be handled by another installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
